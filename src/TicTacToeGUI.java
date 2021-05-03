30ff2eeedea8448d700f80b475d4310630b42a8c
alexibab <alexibanezabril@ginebro.cat>
2021-05-03 19:35:28 +0200

    primera

--- src/TicTacToeGUI.java
+++ src/TicTacToeGUI.java
@@ -139,22 +139,7 @@ public class TicTacToeGUI extends JFrame {
       // 0,2 + 1,1 + 2,0
 
       
-	   if(	
-   			
-	    		(taulell[0][0].getText().equals(jugadorActual) && taulell[0][1].getText().equals(jugadorActual) && taulell[0][2].getText().equals(jugadorActual))   || 
-	    		(taulell[1][0].getText().equals(jugadorActual) && taulell[1][1].getText().equals(jugadorActual) && taulell[1][2].getText().equals(jugadorActual))   ||
-	    		(taulell[2][0].getText().equals(jugadorActual) && taulell[2][1].getText().equals(jugadorActual) && taulell[2][2].getText().equals(jugadorActual))   
-	    		
-	    			
-	    			
-	    			
-	    			) {
-				JOptionPane.showMessageDialog(null, "El jugador " +
-						jugadorActual + " ha guanyat!");
-				tenimGuanyador = true;
-			}
-
-
+	
    }
 
 }
