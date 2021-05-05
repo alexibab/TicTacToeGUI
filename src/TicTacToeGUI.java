6b304f0534725175b63712a14efd2fd7abbaa9d4
alexibab <alexibanezabril@ginebro.cat>
2021-05-05 18:30:17 +0200

    master

--- src/TicTacToeGUI.java
+++ src/TicTacToeGUI.java
@@ -158,35 +158,14 @@ public class TicTacToeGUI extends JFrame {
       // 0,0 + 1,1 + 2,2		
       // 0,2 + 1,1 + 2,0
 
-<<<<<<< HEAD
       
     	   if( (taulell[0][0].getText().equals(jugadorActual) && taulell[0][1].getText().equals(jugadorActual) && taulell[0][2].getText().equals(jugadorActual))   || 
 	    		(taulell[1][0].getText().equals(jugadorActual) && taulell[1][1].getText().equals(jugadorActual) && taulell[1][2].getText().equals(jugadorActual))   ||
-	    		(taulell[2][0].getText().equals(jugadorActual) && taulell[2][1].getText().equals(jugadorActual) && taulell[2][2].getText().equals(jugadorActual)) 	) {
-=======
-         
-	  if(	
-   			
-	    		(taulell[0][0].getText().equals(jugadorActual) && taulell[0][1].getText().equals(jugadorActual) && taulell[0][2].getText().equals(jugadorActual))   || 
-	    		(taulell[1][0].getText().equals(jugadorActual) && taulell[1][1].getText().equals(jugadorActual) && taulell[1][2].getText().equals(jugadorActual))   ||
-	    		(taulell[2][0].getText().equals(jugadorActual) && taulell[2][1].getText().equals(jugadorActual) && taulell[2][2].getText().equals(jugadorActual))   
-	    		
-	    			
-	    			
-	    			
-	    			) {
->>>>>>> 975fbd67c37b831bc645a19af66c4e44cf460016
+	    		(taulell[2][0].getText().equals(jugadorActual) && taulell[2][1].getText().equals(jugadorActual) && taulell[2][2].getText().equals(jugadorActual)) 	) {	    			) {
+
 				JOptionPane.showMessageDialog(null, "El jugador " +
 						jugadorActual + " ha guanyat!");
 				tenimGuanyador = true;
-			}
-
-<<<<<<< HEAD
-
-
-=======
-	
->>>>>>> 975fbd67c37b831bc645a19af66c4e44cf460016
-   }
+			}}
 
 }
