bb3b52743c53a8e087365e51eca5fe013d38a576
alexibab <alexibanezabril@ginebro.cat>
2021-05-05 18:39:44 +0200

    diagonal

--- src/TicTacToeGUI.java
+++ src/TicTacToeGUI.java
@@ -147,7 +147,10 @@ public class TicTacToeGUI extends JFrame {
 	    		
 	    		(taulell[0][0].getText().equals(jugadorActual) && taulell[1][0].getText().equals(jugadorActual) && taulell[2][0].getText().equals(jugadorActual))   || 
 	    		(taulell[0][1].getText().equals(jugadorActual) && taulell[1][1].getText().equals(jugadorActual) && taulell[2][1].getText().equals(jugadorActual))   ||
-	    		(taulell[0][2].getText().equals(jugadorActual) && taulell[1][2].getText().equals(jugadorActual) && taulell[2][2].getText().equals(jugadorActual))   
+	    		(taulell[0][2].getText().equals(jugadorActual) && taulell[1][2].getText().equals(jugadorActual) && taulell[2][2].getText().equals(jugadorActual))   ||
+	    		
+	    		(taulell[0][0].getText().equals(jugadorActual) && taulell[1][1].getText().equals(jugadorActual) && taulell[2][2].getText().equals(jugadorActual))   || 
+	    		(taulell[0][2].getText().equals(jugadorActual) && taulell[1][1].getText().equals(jugadorActual) && taulell[2][0].getText().equals(jugadorActual))   
 	    		
 	    			
 	    			
